00cde71fa9f08a6ee0d1e516a4425a50333d7263
oshvn <oshvndashboard@gmail.com>
2025-09-30 13:19:17 +0700

    chore: resolve supabase wrapper conflict

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,3 @@
 import { supabase as supabaseClient } from "@/integrations/supabase/client";
-
 export const supabase = supabaseClient;
 export type { Database } from "@/integrations/supabase/types";
